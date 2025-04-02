19fd2d8e26eeb48909f00c1e91cdf2557e478a64
bxbi-exe <bxbi442@gmail.com>
2025-04-02 19:31:02 +0200

    Created Spring Boot Application

---
+++ Backend/SmartLibrary/src/test/java/mk/ukim/finki/smartlibrary/SmartLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.smartlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
