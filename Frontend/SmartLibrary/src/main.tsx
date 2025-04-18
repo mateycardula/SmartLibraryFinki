10669b04f40b19e5e116c1b904f12983ba6c79bb
Marija <marija@teamlift.co>
2025-04-19 00:55:48 +0200

    Initial design of home page and auth pages

--- Frontend/SmartLibrary/src/main.tsx
+++ Frontend/SmartLibrary/src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import Home from './pages/home.tsx';
+
+const root = createRoot(document.getElementById('root')!);
+root.render(
+    <StrictMode>
+        <Home />
+    </StrictMode>,
+);
